cfd6bfabd23b20ed4da19d008aca2ffc5c9e27f0
Adrian Nürnberger <adrian@nuernberger.me>
2020-05-20 17:46:20 +0200

    code to reproduce issue

--- app/Http/Livewire/Something.php
+++ app/Http/Livewire/Something.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class Something extends Component
 {
+    public $category_id;
+
+    protected $rules = [
+        'category_id' => 'required|integer',
+    ];
+
+    public function updated($field)
+    {
+        $this->validateOnly($field, $this->rules);
+    }
+
     public function render()
     {
         return view('livewire.something');
--- resources/views/livewire/something.blade.php
+++ resources/views/livewire/something.blade.php
@@ -1,3 +1,13 @@
 <div>
-    //
+    <div x-data x-init="new Choices($refs.select)">
+        <select x-ref="select" wire:model="category_id">
+            <option value="">Select an item</option>
+            <option value="1">Item 1</option>
+            <option value="2">Item 2</option>
+        </select>
+    </div>
+
+    <pre>$category_id: {{ $category_id }}</pre>
+
+    <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js" defer></script>
 </div>
